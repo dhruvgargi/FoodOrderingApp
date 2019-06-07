13a864c2bcd882bde4c3a52d53ba396da2330777
Dhruv Sharma <dhruvgargi@users.noreply.github.com>
2019-06-08 00:11:54 +0530

    Foodies Junction App : Customer Controller and Request/Response models

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @PostMapping(path = "/signup")
+    public ResponseEntity<SignupCustomerResponse> signUpCustomer(@RequestBody SignupCustomerRequest signupCustomerRequest){
+        System.out.println("CustomerController");
+        return new ResponseEntity<SignupCustomerResponse>(HttpStatus.OK);
+    }
+
+}
