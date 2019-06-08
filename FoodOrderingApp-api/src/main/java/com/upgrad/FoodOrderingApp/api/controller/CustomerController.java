a0212a7c2cb0b3eb0458d5c4fb8d8369e4153f79
Dhruv Sharma <dhruvgargi@users.noreply.github.com>
2019-06-08 12:16:49 +0530

    Foodies Junction App : Customer Controller and service class

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.upgrad.FoodOrderingApp.api.controller;
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @PostMapping(path = "/signup")
+    @PostMapping(path = "/signup",consumes = "MediaType.APPLICATION_JSON_UTF8")
     public ResponseEntity<SignupCustomerResponse> signUpCustomer(@RequestBody SignupCustomerRequest signupCustomerRequest){
-        System.out.println("CustomerController");
+        System.out.println("CustomerController>>>>>>"+signupCustomerRequest.toString());
+
         return new ResponseEntity<SignupCustomerResponse>(HttpStatus.OK);
     }
 
---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.upgrad.FoodOrderingApp.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public int signUpCustomerService(){
+         return 1;
+    }
+
+}
